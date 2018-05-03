0e3dfb030f6b6769f97a3fb54162de367ff004df
Chelsea Neto Fernandes <chelsea.fernandes@ocsbstudent.ca>
2018-05-03 10:45:13 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------------------
+-- Title: French Star
+-- Name: Chelsea NF
+-- Course: ICS2O/3C
+-- This program ... 
+
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+display.setStatusBar(display.HiddenStatusBar)
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
---
+++ splash_screen.lua
@@ -0,0 +1,169 @@
+-- splash_screen.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This is the splash screen of the game. It displays the 
+-- company logo that...
+-----------------------------------------------------------------------------------------
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-- Name the Scene
+sceneName = "splash_screen"
+
+-----------------------------------------------------------------------------------------
+
+-- Create Scene Object
+local scene = composer.newScene( sceneName )
+
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+ 
+-- The local variables for this scene
+local stars
+local scrollSpeed = 5
+local scrollXSpeed = 9
+local scrollYSpeed = -4
+local pianoSounds = audio.loadSound("Sounds/pianoEdited.mp3")
+local pianoSoundsChannel
+
+--------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+-- The function that moves the star across the screen
+local function moveStars()
+    stars.x = stars.x + scrollXSpeed
+    stars.y = stars.y + scrollYSpeed
+   
+    -- change the transparency of the logo
+    -- fades out
+    stars.alpha = stars.alpha - 0.01
+   
+    -- rotate the logo 45 degrees again and again
+    stars:rotate( 45 )
+
+    -- scale the image by 200% (x) and 50% (y)
+    stars:scale(1, 1)
+
+    stars.width = stars.width + scrollSpeed
+    stars.height = stars.height + scrollSpeed
+end
+
+
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- set the background to be black
+    display.setDefault("background", 0, 0, 0)
+
+    -- Insert the satrs image
+    stars = display.newImageRect("Images/logo.png", 200, 200)
+
+    -- set the initial x and y position of the stars
+    stars.x = 100
+    stars.y = display.contentHeight/1
+
+    -- Insert objects into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert( stars )
+
+end -- function scene:create( event )
+
+--------------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).
+    if ( phase == "will" ) then
+       
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- start the splash screen music
+        pianoSoundsChannel = audio.play( pianoSounds )
+
+        -- Call the moveStars function as soon as we enter the frame.
+        Runtime:addEventListener("enterFrame", moveStars)
+
+        -- Go to the main menu screen after the given time.
+        timer.performWithDelay ( 3000, gotoMainMenu)          
+        
+    end
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is on screen (but is about to go off screen).
+    -- Insert code here to "pause" the scene.
+    -- Example: stop timers, stop animation, stop audio, etc.
+    if ( phase == "will" ) then  
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called immediately after scene goes off screen.
+    elseif ( phase == "did" ) then
+        
+        -- stop the jungle sounds channel for this screen
+        audio.stop(pianoSoundsChannel)
+    end
+
+end --function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
